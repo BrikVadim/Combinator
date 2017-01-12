16606207525470571eb37bcd8467c61325649477
Vadim Brik <Brik99brik@gmail.com>
2017-01-12 17:01:16 +0600

    Update combinator.js

--- combinator.js
+++ combinator.js
@@ -1,4 +1,7 @@
-const factorial = n => {
+const P = factorial
+const arrange = (args) => Array.prototype.sort.call(args, (a, b) => +a < +b)
+
+function factorial(n) {
   if (n > 0 && n <= 170) {
     return n * factorial(n - 1)
   } else if (n === 0) {
@@ -8,14 +11,23 @@ const factorial = n => {
   }
 }
 
-const P = factorial
-
-const A = (n, m) => {
-  [n, m] = n > m ? [n, m] : [m, n]
+function A(n, m) {
+  arrange(arguments)
   return factorial(n) / factorial(n - m)
 }
 
-const C = (n, m) => {
-  [n, m] = n > m ? [n, m] : [m, n]
+function C(n, m) {
+  arrange(arguments)
   return factorial(n) / (factorial(m) * factorial(n - m))
+}
+
+function Bernoulli(n, m, p, q) {
+  [n, m] = n > m ? [n, m] : [m, n]
+  return C(n, m) * p ** m * q ** (n - m)
+}
+
+function BernoulliAtRange(n, m, p, q) {
+  let result = []
+  for (;m != 0; m--) result.push(Bernoulli(n, m, p, q))
+  return result
 }
