4439c69f8b79285da61d3bdb93b87d45638907c6
Vadim Brik <Brik99brik@gmail.com>
2017-01-30 21:04:45 +0600

    Update combinator.js

--- combinator.js
+++ combinator.js
@@ -1,32 +1,36 @@
-const P = factorial
-const arrange = (args) => Array.prototype.sort.call(args, (a, b) => +a < +b)
+const arrange = args => Array.prototype.sort.call(args, (a, b) => +a < +b)
 
-function factorial(n) {
-  if (n > 0 && n <= 170) {
-    return n * factorial(n - 1)
-  } else if (n === 0) {
-    return 1
-  } else {
-    throw new RangeError('Параметр должен быть положительным числом не больше 170');
+const factorial = n => {
+  const iterator = (current, acc) => {
+    if (current > 0) {
+      return iterator(current - 1, acc * current)
+    } else if (current === 0) {
+      return acc
+    } else {
+      throw new RangeError('The function argument must be a positive number');
+    }
   }
+  return iterator(n, 1)
 }
 
-function A(n, m) {
+const P = factorial
+
+const A = (n, m) => {
   arrange(arguments)
   return factorial(n) / factorial(n - m)
 }
 
-function C(n, m) {
+const C = (n, m) => {
   arrange(arguments)
   return factorial(n) / (factorial(m) * factorial(n - m))
 }
 
-function Bernoulli(n, m, p, q) {
+const Bernoulli = (n, m, p, q) => {
   [n, m] = n > m ? [n, m] : [m, n]
   return C(n, m) * p ** m * q ** (n - m)
 }
 
-function BernoulliAtRange(n, m, p, q) {
+const BernoulliAtRange = (n, m, p, q) => {
   let result = []
   for (;m != 0; m--) result.push(Bernoulli(n, m, p, q))
   return result
